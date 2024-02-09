e27136f928eadd41397780b79cba12eebf774a0e
Jeffery Kimbrow <71730781+kimbrow-slice@users.noreply.github.com>
2024-02-08 18:11:23 -0600

    Update functions.h
    Comments in code

--- functions.h
+++ functions.h
@@ -2,24 +2,32 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include <utility>
-#include <string>
+#include <utility> // Used utility for pair function std::pair 
+#include <string> 
 using namespace std;
 
-// Initial Array Size
-const int INITIAL_MAX_SIZE = 100;
-extern int* arr;
-extern int currentSize;
-extern int currentCapacity;
 
-void doubleArraySize();
-void loadData(const char* filename);
-void printArray();
-int find(int num);
+const int INITIAL_MAX_SIZE = 100; // Initial Array Size
+extern int* arr; // Pointer to the array of integers
+extern int currentSize; // Current number of elements in the array
+extern int currentCapacity; // Current maximum capacity of the array
+
+void doubleArraySize(); // Double size as needed
+void loadData(const char* filename); // Read in file
+void printArray(); // Display user the array after updating
+
+int find(int num); 
 // Using Pair to return two old and new values from a our function 
 pair<int, int> modify(int index, int newValue);
-bool add(int num);
-bool removeOrReplace(int index, bool remove = true);
+
+bool add(int num); // Add a number, return true if number is successfully added
+
+/* Remove or replace an elemetn at given index.
+Remove is true for removal, false for replacing. */
+bool removeOrReplace(int index, bool remove = true); 
+
+/* Validate if input is a positive whole number, return true if string is positive whole number
+return false otherwise */
 bool isPosWholeNum(const string& str);
 
 #endif // FUNCTIONS_H
